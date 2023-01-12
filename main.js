ddf67193220c8fc77e98de86df6cf2e6ce7a866d
iliaroozban <94915568+iliaroozbann@users.noreply.github.com>
2023-01-12 15:54:33 +0330

    Added some features and edited base

--- main.js
+++ main.js
@@ -2,27 +2,40 @@ const r = true;
 const playbtn = document.querySelector(".playme");
 const playBtnChangestoPause = document.querySelector(".playlogo")
 const music = document.querySelector("#player")
+const rangePlaying = document.querySelector("#dur");
 var isPlaying = false;
 var filling = false ;
-
+var intervalId = null;
 const unheart = document.querySelector(".heart")
 
 playbtn.addEventListener("click" , togglePlay);
 function togglePlay() {
-   isPlaying ? music.pause() : music.play();
+    isPlaying = !isPlaying;
+    isPlaying ? pauseMusic() : playMusic();
  };
+
+
+ const pauseMusic = () => {
+  music.pause()
+  playBtnChangestoPause.src = "./svgs/play-svgrepo-com.svg" ;
+  if(intervalId != null) {
+    clearInterval(intervalId);
+  }
+ }
  
- music.onplaying = function() {
-   isPlaying = true;
-   playBtnChangestoPause.src = "./svgs/pause-svgrepo-com.svg" ;
- };
- music.onpause = function() {
-   isPlaying = false;
-   playBtnChangestoPause.src = "./svgs/play-svgrepo-com.svg" ;
- };
+const playMusic = () => {
+  music.play();
+  playBtnChangestoPause.src = "./svgs/pause-svgrepo-com.svg" ;
+  intervalId = setInterval(()=>{
+    const duri = music.duration ;
+    const maxin = rangePlaying.max ;
+    rangePlaying.max = duri ;
+    const dur = +rangePlaying.value++  
+   },1000)
+}
+
 
  unheart.addEventListener("click" , ()=>{
-
        if(filling === false){
         unheart.src = "./svgs/heart-filled-svgrepo-com.svg"
         filling = true;
@@ -37,13 +50,3 @@ function togglePlay() {
        }
        
  })
-
- 
-
- 
-   
-    
-   
-
-
-
