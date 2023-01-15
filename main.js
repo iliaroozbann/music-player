f5d8a9f49a9595949fb1fc2a53434bdc3652aff4
iliaroozban <94915568+iliaroozbann@users.noreply.github.com>
2023-01-15 12:17:48 +0330

    add new features

--- main.js
+++ main.js
@@ -7,6 +7,23 @@ var isPlaying = false;
 var filling = false ;
 var intervalId = null;
 const unheart = document.querySelector(".heart")
+const pict = document.querySelector(".cover");
+const songname = document.querySelector(".song-name");
+const artistname = document.querySelector(".artist-name");
+const next = document.querySelector(".next");
+const prev =document.querySelector(".prev");
+
+
+
+
+//sourses
+
+const songn = ["la espada" , "TV" , "snowfall"];
+const artists = ["eternal raijin" , "bilie eilish" , "Oneheart"];
+const coverad= ["./imgas/500x500.jpg" , "./imgas/tv.jpg" , "./imgas/d0b6e07bfad9fee7a210e212b1a3da3f.jpg"];
+const songsrc = ["./music/eternal_raijin_la_espada 128.mp3" , "./music/billie_eilish_-_tv.mp3","./music/Snowfall_sped_up__D7Vx6srE3Vk_140.mp3" ];
+
+
 
 playbtn.addEventListener("click" , togglePlay);
 function togglePlay() {
@@ -28,11 +45,139 @@ const playMusic = () => {
   playBtnChangestoPause.src = "./svgs/pause-svgrepo-com.svg" ;
   intervalId = setInterval(()=>{
     const duri = music.duration ;
-    const maxin = rangePlaying.max ;
+    let maxin = rangePlaying.max ;
     rangePlaying.max = duri ;
-    const dur = +rangePlaying.value++  
+    const dur = +rangePlaying.value++;
+    if(rangePlaying.value == rangePlaying.max){
+      clearInterval(intervalId);
+      rangePlaying.value = "0";
+      playBtnChangestoPause.src = "./svgs/play-svgrepo-com.svg" ;
+    }
+ 
    },1000)
+   
+
 }
+
+   
+   var i = 0 ;
+
+  //1 
+      music.addEventListener("ended", () =>{
+        clearInterval(intervalId);
+        
+        rangePlaying.value = "0" ;
+        playBtnChangestoPause.src = "./svgs/play-svgrepo-com.svg" ;
+        songname.innerText = songn[i];  
+        artistname.innerText = artists[i];  
+        music.src = songsrc[i];
+        pict.src = coverad[i];
+        if(i==3){
+          i = 0;
+          playBtnChangestoPause.src = "./svgs/pause-svgrepo-com.svg" ;
+          songname.innerText = songn[i];  
+          artistname.innerText = artists[i];  
+          music.src = songsrc[i];
+          pict.src = coverad[i];
+          
+          }
+          i++
+          
+           
+          intervalId = setInterval(()=>{
+           const duri = music.duration ;
+           let maxin = rangePlaying.max ;
+           rangePlaying.max = duri ;
+           const dur = +rangePlaying.value++;
+           if(rangePlaying.value == rangePlaying.max){
+             clearInterval(intervalId);
+             rangePlaying.value = "0";
+             playBtnChangestoPause.src = "./svgs/play-svgrepo-com.svg" ;
+           }
+        
+          },1000)
+         })
+
+//1
+
+
+//2
+         next.addEventListener("click", () =>{
+          clearInterval(intervalId);
+          rangePlaying.value = "0" ;
+          music.autoplay = true;
+          playBtnChangestoPause.src = "./svgs/pause-svgrepo-com.svg" ;
+          songname.innerText = songn[i];  
+          artistname.innerText = artists[i];  
+          music.src = songsrc[i];
+          pict.src = coverad[i];
+          if(i==3){
+           i = 0;
+           playBtnChangestoPause.src = "./svgs/pause-svgrepo-com.svg" ;
+           songname.innerText = songn[i];  
+           artistname.innerText = artists[i];  
+           music.src = songsrc[i];
+           pict.src = coverad[i];
+           
+           }
+           i++
+           
+            
+           intervalId = setInterval(()=>{
+            const duri = music.duration ;
+            let maxin = rangePlaying.max ;
+            rangePlaying.max = duri ;
+            const dur = +rangePlaying.value++;
+            if(rangePlaying.value == rangePlaying.max){
+              clearInterval(intervalId);
+              rangePlaying.value = "0";
+              playBtnChangestoPause.src = "./svgs/play-svgrepo-com.svg" ;
+            }
+         
+           },1000)
+           })
+
+//2
+
+//3 
+
+prev.addEventListener("click", () =>{
+  clearInterval(intervalId);
+  rangePlaying.value = "0" ;
+  music.autoplay = true;
+  playBtnChangestoPause.src = "./svgs/pause-svgrepo-com.svg" ;
+  i == 0;
+    
+      if(i === 0){
+      i = 3;
+      }
+  
+      i--;
+      songname.innerText = songn[i];  
+      artistname.innerText = artists[i];  
+      music.src = songsrc[i];
+      pict.src = coverad[i];
+      
+       
+    
+  
+   intervalId = setInterval(()=>{
+    const duri = music.duration ;
+    let maxin = rangePlaying.max ;
+    rangePlaying.max = duri ;
+    const dur = +rangePlaying.value++;
+    if(rangePlaying.value == rangePlaying.max){
+      clearInterval(intervalId);
+      rangePlaying.value = "0";
+      playBtnChangestoPause.src = "./svgs/play-svgrepo-com.svg" ;
+    }
+ 
+   },1000)
+   })
+
+     
+
+
 
 
  unheart.addEventListener("click" , ()=>{
@@ -50,3 +195,10 @@ const playMusic = () => {
        }
        
  })
+
+
+
+
+
+
+ 
